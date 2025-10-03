3492bf44211468db85cd2bd7da24e1355482c8c3
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-10-04 00:11:14 +0200

    Import af billeder og opret containers

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,10 +2,49 @@ import { NavLink } from "react-router";
 
 export default function Nav() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-    </nav>
+    <>
+      <header className="flex justify-between w-[100%] px-50 mt-5">
+        <div>
+          <NavLink to="/">
+            <img src="public/img/icons/bh-media-logo.svg" alt="" />
+          </NavLink>
+        </div>
+
+        <nav className="flex justify-center gap-4">
+          <NavLink to="/">Prices</NavLink>
+          <NavLink to="/events">Events</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/recommendations">recommendations</NavLink>
+        </nav>
+
+        <div className="flex gap-3">
+          <a
+            className="transition-transform duration-200 hover:scale-125"
+            href="https://www.instagram.com/bjarkeholm.media/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="w-8 "
+              src="/img/icons/IG-icon.svg"
+              alt="Instagram"
+            />
+          </a>
+
+          <a
+            className="transition-transform duration-200 hover:scale-125"
+            href="https://www.linkedin.com/in/bjarke-holm-7409051a2/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="w-8"
+              src="/img/icons/LinkedIn-icon.svg"
+              alt="LinkedIn"
+            />
+          </a>
+        </div>
+      </header>
+    </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,64 @@
 export default function HomePage() {
   return (
-    <main className="bg- w-[100%] flex justify-self-center px-50">
-      <section className="bg-() w-[100vw] h-[50vh]">
-        <h1>
-          Welcome <br />
-          to my world of visuals
-        </h1>
+    <main className="flex flex-col justify-center w-[100vw] px-50 mt-10">
+      <section className="w-[100%] h-[50vh]">
+        <div className="justify-start text-stone-600 text-md font-medium font-['Inter']">
+          <h1>
+            Welcome <br />
+            to my world of visuals
+          </h1>
+        </div>
 
-        <h1>
-          Scroll down to explore <br />
-          my aesthetics
-        </h1>
+        <div className="bg-black aspect-video mt-[2rem] w-[100%] h-[50vh] rounded-xl overflow-hidden">
+          <img
+            className="object-cover w-full h-full"
+            src="/img/Vorupør_Edits/Klit_siluette_Mobile.jpg"
+            alt="klit ved vesterhavet"
+          />
+        </div>
+
+        <div className="flex justify-end mt-[2rem]text-stone-600 text-md font-medium font-['Inter'] mt-[2rem] ">
+          <h1>
+            Scroll down to explore <br />
+            my aesthetics
+          </h1>
+        </div>
+      </section>
+
+      <section className="grid grid-cols-1 w-[100%] h-[80vh] mt-[30rem] mb-[30rem]">
+        <div className="">
+          <h2>
+            Architecture - <em> I love it</em>
+          </h2>
+        </div>
+
+        {/*Image container */}
+        <div className="grid grid-cols-4 bg-() w-[100%] h-[100%]  overflow-hidden rounded-xl ">
+          <div className="w-80 h-[584px] relative">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Uni.jpg"
+            />
+          </div>
+          <div className=" w-80 h-[584px] relative">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Rosensgade-2.jpg"
+            />
+          </div>
+          <div className="w-80 h-[584px] relative">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Chair.jpg"
+            />
+          </div>
+          <div className="w-80 h-[584px] relative ">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Street-3.jpg"
+            />
+          </div>
+        </div>
       </section>
     </main>
   );
