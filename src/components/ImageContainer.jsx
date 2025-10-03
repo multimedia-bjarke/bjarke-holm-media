53b2b79f65ad4e0f8858ff0640896ba2b29b1f08
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-10-04 00:31:15 +0200

    starting up the imagecontainer component

---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,5 @@
+import { useEffect } from "react";
+
+export default function ImageContainer(props) {
+  return <h1>{props.name}</h1>;
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import ImageContainer from "../components/ImageContainer";
+
 export default function HomePage() {
   return (
     <main className="flex flex-col justify-center w-[100vw] px-50 mt-10">
@@ -25,8 +27,56 @@ export default function HomePage() {
         </div>
       </section>
 
-      <section className="grid grid-cols-1 w-[100%] h-[80vh] mt-[30rem] mb-[30rem]">
+      {/*Section 1 - Architecture */}
+      <section className="grid grid-cols-1 w-[100%] h-[80vh] mt-[30rem]">
         <div className="">
+          <h2>
+            Architecture - <em> I love it</em>
+          </h2>
+        </div>
+
+        <ImageContainer />
+
+        {/*Image container */}
+        <div className="grid grid-cols-4 bg-() w-[100%] h-[100%]  overflow-hidden rounded-xl ">
+          <div className="w-80 h-[584px] relative">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Uni.jpg"
+            />
+          </div>
+          <div className=" w-80 h-[584px] relative">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Rosensgade-2.jpg"
+            />
+          </div>
+          <div className="w-80 h-[584px] relative">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Chair.jpg"
+            />
+          </div>
+          <div className="w-80 h-[584px] relative ">
+            <img
+              className="w-80 h-[585px] left-[-11px] top-0 absolute"
+              src="/img/fotografi/Street-3.jpg"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/*Section 2 - Tools */}
+      <section className="grid grid-cols-1 w-[100%] h-auto mt-[10rem] justify-items-center">
+        <div className="">
+          <h3>I can't do much without these</h3>
+        </div>
+      </section>
+
+      {/*Section 3 - Nature */}
+      <section className="grid grid-cols-1 w-[100%] h-[80vh] mt-[10rem] mb-[30rem]">
+        <div className="">
+          <h1 className="text-stone-600 text-md font-medium">Keep going</h1>
           <h2>
             Architecture - <em> I love it</em>
           </h2>
